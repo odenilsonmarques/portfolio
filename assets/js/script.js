968ca4934c981707b240e9284a23bd540c43c381
odenilsonmarques <odmarquesgm@gmail.com>
2023-09-05 10:37:15 -0300

    menu hamburguer finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-function clickMenu(){
-    if(menu.style.display == 'block'){
-       menu.style.display = 'none'
-    }else{
-      menu.style.display = 'block'
-    }
+function clickMenu() { 
+   
+   if(menu.style.display == 'block'){
+      menu.style.display = 'none'
+   }else{
+    menu.style.display = 'block'
+   }
+    
   }
 
   
