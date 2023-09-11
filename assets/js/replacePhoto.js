bdb29e7adcc08fe1e41841145e636c33ca86e51f
odenilsonmarques <odmarquesgm@gmail.com>
2023-09-11 09:38:50 -0300

    new feature

---
+++ assets/js/replacePhoto.js
@@ -0,0 +1,14 @@
+// Função para substituir a imagem quando a largura da tela for menor ou igual a 575px
+function substituirImagem() {
+    const larguraTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (larguraTela <= 575) {
+        document.getElementById("imagemSlide").src = "assets/img/portfolio2.png";
+    } else {
+        document.getElementById("imagemSlide").src = "assets/img/financeiro2.jpeg";
+    }
+}
+
+// Chame a função quando a página for carregada e redimensionada
+window.addEventListener("load", substituirImagem);
+window.addEventListener("resize", substituirImagem);
