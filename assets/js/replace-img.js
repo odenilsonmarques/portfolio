6cb8c02ed0ee6f76809b0d7a1d427369b1890e22
odenilsonmarques <odmarquesgm@gmail.com>
2023-09-13 12:28:24 -0300

    image responsive

--- assets/js/replace-img.js
+++ assets/js/replace-img.js
@@ -3,10 +3,19 @@ function substituirImagem() {
     const larguraTela = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
     if (larguraTela <= 575) {
-        document.getElementById("imagemSlide").src = "assets/img/financeiro1.png";
-    } else {
-        document.getElementById("imagemSlide").src = "assets/img/financeiro2.jpeg";
-    }
+        document.getElementById("imagemSlide").src = "assets/img/financeiro2.png";
+
+        document.getElementById("imagemSlide1").src = "assets/img/loja2.jpeg";
+
+        document.getElementById("imagemSlide2").src = "assets/img/tgc2.jpeg";
+    } 
+    // else {
+    //     document.getElementById("imagemSlide").src = "assets/img/financeiro1.jpeg";
+
+    //     document.getElementById("imagemSlide").src = "assets/img/loja1.jpeg";
+
+    //     document.getElementById("imagemSlide").src = "assets/img/tgc1.jpeg";
+    // }
 }
 
 // Chame a função quando a página for carregada e redimensionada
